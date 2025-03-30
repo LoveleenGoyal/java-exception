c14ddf49e631565d7df5e0ad3468dae7b50e93c6
LoveleenGoyal <loveleengoyal26@gmail.com>
2025-03-30 21:47:27 +0530

    [Loveleen] ADD: java user defined exception

---
+++ src/main/java/com/bridgelabz/exceptions/AgeValidation.java
@@ -0,0 +1,35 @@
+package com.bridgelabz.exceptions;
+
+import java.util.Scanner;
+
+class InvalidAgeException extends Exception {
+    public InvalidAgeException(String message) {
+        super(message);
+    }
+}
+
+public class AgeValidation {
+    static void validAge(int age) throws InvalidAgeException{
+        if(age<18){
+            throw new InvalidAgeException("UNDER AGE- should be 18 above");
+        }
+        System.out.println("Access granted!");
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try {
+            System.out.print("Enter your age: ");
+            int age = sc.nextInt();
+            validAge(age);
+        } catch (InvalidAgeException e) { // Handling custom exception
+            System.out.println(e.getMessage());
+        } finally {
+            sc.close();
+        }
+    }
+}
+
+/*Enter your age: 13
+UNDER AGE- should be 18 above*/
